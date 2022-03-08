e7f049d05665312878a7825b41ca57b808c69b1b
batumutsu <batumutsu@gmail.com>
2022-03-08 15:05:16 +0200

    final changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from "./components/Card"
+//import player from "./images/player.png"
+import data from './data';
 
 function App() {
+  const cards = data.map(x =>
+                          <Card 
+                          key = {x.id}
+                          {...x}
+                          //x = {x} similar to above
+                          />
+                        )
   return (
     <div>
       <Navbar />
       <Hero />
+      <section className="cards-list">
+        {cards}
+      </section>
+      
     </div>
   );
 }
